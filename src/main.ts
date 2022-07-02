3dbb4c13d707e0a7a9b5f1282eefa29e771d1c63
w-haibara <hwhaibarawataru@gmail.com>
2022-07-03 02:09:57 +0900

    remove unused packages

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-
 loadFonts();
 
 createApp(App).use(vuetify).mount("#app");
